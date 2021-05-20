93797eaaebfe7e1c99dd9676bebd0b1149fbcf9d
Arthur <jorisplayz@gmail.com>
2021-05-20 22:23:58 +0200

    Basic folder setup
    Added Linter, first working command.

---
+++ FriendsINC.js
@@ -0,0 +1,39 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+const fs = require('fs');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+
+client.on('message', message => {
+	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+    }
+    catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+
+
+  client.login(config.token);
